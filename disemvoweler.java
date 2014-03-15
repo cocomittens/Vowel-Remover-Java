d9cf8bdbf4367f08b88937104da08af1ce065e6b
Corrie <cgripens@ucsc.edu>
2014-03-15 01:36:09 -0700

    disemvoweler

---
+++ disemvoweler.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class disemvoweler{
+
+	public static boolean isVowel(String c){
+		return "aeiou".indexOf(c) != -1;
+	}
+
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		String s = scan.nextLine();
+		String newS = "", vowelS="", ch="";
+		for(int i=0; i<s.length(); i++){
+			ch = s.substring(i, i+1);
+			if(isVowel(ch))
+				vowelS += ch;
+			else
+				newS += ch;			
+		}
+		System.out.println(newS);
+		System.out.println(vowelS);
+	}
+}
